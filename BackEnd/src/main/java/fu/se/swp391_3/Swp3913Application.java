db882623e2b81a1babed5bba740902c1739a853d
huyga154 <huyga154@gmail.com>
2024-09-22 15:32:10 +0700

    Create Back End Project Base Src file

---
+++ BackEnd/src/main/java/fu/se/swp391_3/Swp3913Application.java
@@ -0,0 +1,13 @@
+package fu.se.swp391_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Swp3913Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Swp3913Application.class, args);
+    }
+
+}
