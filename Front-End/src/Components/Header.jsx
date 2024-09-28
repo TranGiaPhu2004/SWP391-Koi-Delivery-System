40038adf78c3d0ac81343aa62d7b0ca7cc26533f
TranGiaPhu2004 <trangiaphuc0502@gmail.com>
2024-09-28 20:07:30 +0700

    ok

--- Front-End/src/Components/Header.jsx
+++ Front-End/src/Components/Header.jsx
@@ -1,13 +1,17 @@
-
 import React from 'react';
 import './Header.css'; // Bạn có thể thêm CSS vào file riêng
 import logo from '../assets/image/vietnam.png'
 import logo1 from '../assets/image/japan.png'
 import logo3 from '../assets/image/Logo.png'
-
+import Login from '../Page/Login';
+import { Link } from "react-router-dom";
 
 function Header() {
-  
+  function goLogin(){
+      return(
+        <Login></Login>
+      )
+  }
   return (
     <header>
       
@@ -27,7 +31,9 @@ function Header() {
           <a href="/news">News</a>
         </nav>
         <div className="auth-buttons">
-          <button >Sign In</button>
+        <Link to="/login">
+        <button>Login</button>
+      </Link>
           <button>Register</button>
         </div>
       </div>
