dd2c885d06394bed8f5ae2f7271f4e6d1afe40b4
TranGiaPhu2004 <trangiaphuc0502@gmail.com>
2024-09-28 15:47:10 +0700

    ok

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -1,14 +1,17 @@
 
 import Home from './Page/Home/Home';
 import './App.css';
+import Login from './Page/Login';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <>
-
-      <Home></Home>
-
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
 
   );
 }
--- Front-End/src/Components/Header.jsx
+++ Front-End/src/Components/Header.jsx
@@ -5,6 +5,7 @@ import logo from '../assets/image/vietnam.png'
 import logo1 from '../assets/image/japan.png'
 import logo3 from '../assets/image/Logo.png'
 import Login from '../Page/Login';
+import { Link } from "react-router-dom";
 
 function Header() {
   function goLogin(){
@@ -31,7 +32,9 @@ function Header() {
           <a href="/news">News</a>
         </nav>
         <div className="auth-buttons">
-          <button onClick={goLogin}>Sign In</button>
+        <Link to="/login">
+        <button>Login</button>
+      </Link>
           <button>Register</button>
         </div>
       </div>
