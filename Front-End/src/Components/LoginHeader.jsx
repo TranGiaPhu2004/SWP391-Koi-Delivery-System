c9d72795da1a91f9f48900c501197c5003076a95
Nerodamonte <nerodamonte@gmail.com>
2024-09-28 16:36:57 +0700

    Update Login

--- Front-End/src/Components/LoginHeader.jsx
+++ Front-End/src/Components/LoginHeader.jsx
@@ -4,10 +4,33 @@ import LogoLogin from '../assets/image/LogoLogin.png'
 function LoginHeaderMethod(){
 return(
     <div className = "main">
-        <img src={LogoLogin} alt="LogoLogin" />
-       <img  className="Logo" src={Logo} alt="Logo" />
-       
+        <div className='LogoLogin'>
+             <img src={LogoLogin} alt="LogoLogin" className='LogoLoginImg' />
+        </div>
+        <div className="login-form">
+        <div className="logo">
+          <img src={Logo} alt="Logo" />
+        </div>
+        <h1>LOGIN PAGE</h1>
+        <form>
+          <div className="input-group">
+            <label>USERNAME</label>
+            <input type="text" placeholder="Enter your username" />
+          </div>
+          <div className="input-group">
+            <label>PASSWORD</label>
+            <input type="password" placeholder="Enter your password" />
+          </div>
+          <div className="links">
+            <a href="/forgot-password">Forgot password?</a>
+            <a href="/register">Register here.</a>
+          </div>
+          <button type="submit">LOGIN</button>
+        </form>
+      </div>
+
     </div>
+    
 );
 }
 export default LoginHeaderMethod
