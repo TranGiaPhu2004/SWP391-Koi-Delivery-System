5173c3190cd462f172bb117273280dda0beec1b9
Tyloos <manhvu5000@gmail.com>
2024-09-30 13:02:34 +0700

    Update App.jsx

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -1,14 +1,12 @@
 
-// import Home from './Page/Home/Home';
+import Home from './Page/Home/Home';
 import './App.css';
-import Manager from './Page/Manager/Manager';
 
 function App() {
   return (
     <>
 
-      {/* <Home></Home> */}
-      <Manager/>
+       <Home></Home> 
     
     </>
 
