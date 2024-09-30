f60cf809567de00510a69aecea76f6074a99f785
Trần Tuấn Anh <nguyentrantuananhbeo@gmail.com>
2024-09-30 15:18:47 +0700

    Login-Register-Forgot Password (Trung Nguyen)

---
+++ Front-End/src/Page/Register/Register.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import RegisterContent from "../../Components/RegisterContent.jsx";
+function Register() {
+  return <RegisterContent></RegisterContent>;
+}
+
+export default Register;
+
