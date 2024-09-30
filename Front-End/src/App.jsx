886cafad676e93ae039160c86d895e8c7041ff8c
Tyloos <manhvu5000@gmail.com>
2024-09-30 13:24:54 +0700

    managerCustomer

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -1,17 +1,18 @@
-import Home from './Page/Home/Home';
+// import Home from './Page/Home/Home';
 import './App.css';
-import Login from './Page/Login/Login';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import Login from './Page/Login/Login';
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Manager from './Page/Manager/Manager'
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </Router>
-
+    // <Router>
+    //   <Routes>
+    //     <Route path="/" element={<Home />} />
+    //     <Route path="/login" element={<Login />} />
+    //   </Routes>
+    // </Router>
+    <Manager/>
   );
 }
 
---
+++ Front-End/src/Page/Manager/Manager.jsx
@@ -0,0 +1,11 @@
+import ManagerCustomer from '../../Components/ManagerCustomer.jsx'
+
+function Manager() {
+return(
+    <>  
+    <ManagerCustomer/>
+    </>
+);
+
+}
+export default Manager
