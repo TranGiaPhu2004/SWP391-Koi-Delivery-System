4a75e7374b6e655b18c631201de89350a4e3ff5a
Trần Tuấn Anh <nguyentrantuananhbeo@gmail.com>
2024-10-01 20:32:31 +0700

    View-Order (+ - update)

--- Front-End/src/Components/OrderInformation.jsx
+++ Front-End/src/Components/OrderInformation.jsx
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 import './OrderInformation.css'
 import pickoi1 from '../assets/image/pickoi1.png'
 import pickoi2 from '../assets/image/pickoi2.png'
@@ -7,27 +8,75 @@ import pickoi3 from '../assets/image/pickoi3.png'
 
 const OrderInformation = ({ totalOrder, deliveryCost, totalAmount, deliveryTo }) => {
 
+
+    const [count1, setCount1] = useState(0);
+    const [count2, setCount2] = useState(0);
+    const [count3, setCount3] = useState(0);
+
+
+    const increment1 = () => {
+        setCount1((c) => c + 1);
+    }
+    const increment2 = () => {
+        setCount2((c) => c + 1);
+    }
+    const increment3 = () => {
+        setCount3((c) => c + 1);
+    }
+    const decrement1 = () => {
+        if (count1 > 0) {
+            setCount1((c) => c - 1);
+        } else {
+            alert('Sorry, you could not decrement ...')
+        }
+    }
+    const decrement2 = () => {
+        if (count2 > 0) {
+            setCount2((c) => c - 1);
+        } else {
+            alert('Sorry, you could not decrement ...')
+        }
+    }
+    const decrement3 = () => {
+        if (count3 > 0) {
+            setCount3((c) => c - 1);
+        } else {
+            alert('Sorry, you could not decrement ...')
+        }
+    }
+
+    const Delete1 = () => {
+        setCount1(0);
+    }
+    const Delete2 = () => {
+        setCount2(0);
+    }
+    const Delete3 = () => {
+        setCount3(0);
+    }
+
     return (
         <div className="OrderInformation-main-order">
             <div className="OrderInformation-main-pic">
                 <div className="OrderInformation-pic-koi1">
                     <img src={pickoi1} alt="PIC KOI 1" />
                     <div className="OrderInformation-pic1">
-                        <p>Small Box</p>
-                        <button className='OrderInformation-button'>Delete</button>
+                        <p>Big Box</p>
+                        <button className='OrderInformation-button' onClick={Delete1}>Delete</button>
                     </div>
-                    <div className="OrderInformation-amount">
-                        <button>
+                    <div className="OrderInformation-amount1">
+                        <button className={`decrement-button1 ${count1 > 0 ? 'active' : 'blurred'}`}
+                            onClick={decrement1}>
                             -
                         </button>
                         <button>
-                            1
+                            {count1}
                         </button>
-                        <button>
+                        <button onClick={increment1}>
                             +
                         </button>
 
-                        
+
                     </div>
                 </div>
 
@@ -35,17 +84,18 @@ const OrderInformation = ({ totalOrder, deliveryCost, totalAmount, deliveryTo })
                 <div className="OrderInformation-pic-koi2">
                     <img src={pickoi2} alt="PIC KOI 2" />
                     <div className="OrderInformation-pic1">
-                        <p>Small Box</p>
-                        <button className='OrderInformation-button'>Delete</button>
+                        <p>Traditional Box</p>
+                        <button className='OrderInformation-button' onClick={Delete2}>Delete</button>
                     </div>
-                    <div className="OrderInformation-amount">
-                        <button>
+                    <div className="OrderInformation-amount2">
+                        <button className={`decrement-button2 ${count2 > 0 ? 'active' : 'blurred'}`}
+                            onClick={decrement2}>
                             -
                         </button>
                         <button>
-                            1
+                            {count2}
                         </button>
-                        <button>
+                        <button onClick={increment2}>
                             +
                         </button>
                     </div>
@@ -54,16 +104,17 @@ const OrderInformation = ({ totalOrder, deliveryCost, totalAmount, deliveryTo })
                     <img src={pickoi3} alt="PIC KOI 3" />
                     <div className="OrderInformation-pic1">
                         <p>Small Box</p>
-                        <button className='OrderInformation-button'>Delete</button>
+                        <button className='OrderInformation-button' onClick={Delete3}>Delete</button>
                     </div>
-                    <div className="OrderInformation-amount">
-                        <button>
+                    <div className="OrderInformation-amount3">
+                        <button className={`decrement-button3 ${count3 > 0 ? 'active' : 'blurred'}`}
+                            onClick={decrement3}>
                             -
                         </button>
                         <button>
-                            1
+                            {count3}
                         </button>
-                        <button>
+                        <button onClick={increment3}>
                             +
                         </button>
                     </div>
@@ -75,16 +126,16 @@ const OrderInformation = ({ totalOrder, deliveryCost, totalAmount, deliveryTo })
                 <div className='OrderInformation-order-details'>
                     <h2>Order Information</h2>
                     <div className='OrderInformation-line'></div>
-                    <p>Total order: {totalOrder} vnđ</p>
+                    <p>Total order: {totalOrder} {(count1 + count2) * parseFloat(1000000)} vnđ</p>
                     <p>Delivery Cost: {deliveryCost} vnđ</p>
-                    <span>Total Amount: {totalAmount} vnđ</span>
+                    <span>Total Amount: {totalAmount} {(count1 + count2) * parseFloat(1000000)} vnđ</span>
                     <p>Delivery to: {deliveryTo}</p>
                     <p>By pressing the button, I agree to the <a href="#">Terms and Conditions.</a></p>
                     <button className="OrderInformation-payment-button">{totalAmount} vnđ Payment <a href="#"><span>→</span></a></button>
-                    
-                <div className="OrderInformation-cancel">
-                    <a href='#'>Delete</a>
-                </div>
+
+                    <div className="OrderInformation-cancel">
+                        <a href='#'>Delete</a>
+                    </div>
                 </div>
 
             </div>
