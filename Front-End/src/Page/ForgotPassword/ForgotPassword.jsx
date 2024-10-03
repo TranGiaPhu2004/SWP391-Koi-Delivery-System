e8995aa107f72e78cf6a99713d68c41a4612434e
huyga154 <huyga154@gmail.com>
2024-10-03 16:11:37 +0700

    revert 6 files

---
+++ Front-End/src/Page/ForgotPassword/ForgotPassword.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ForgotPasswordContent from "../../Components/ForgotPasswordContent.jsx";
+
+function ForgotPassword() {
+  return <ForgotPasswordContent></ForgotPasswordContent>;
+}
+
+export default ForgotPassword;
+
