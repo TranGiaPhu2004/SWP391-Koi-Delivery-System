8e1c28bb7c7a65e8713b0eea0d6659d01cc8e859
Trần Tuấn Anh <nguyentrantuananhbeo@gmail.com>
2024-10-22 16:07:25 +0700

    ViewOrder(Completed)
    ok

--- Front-End/src/Components/OrderInformation.jsx
+++ Front-End/src/Components/OrderInformation.jsx
@@ -1,89 +1,97 @@
-
 import { useState } from 'react';
-import './OrderInformation.css'
-import pickoi1 from '../assets/image/pickoi1.png'
-import pickoi2 from '../assets/image/pickoi2.png'
-import pickoi3 from '../assets/image/pickoi3.png'
-
+import './OrderInformation.css';
+import pickoi1 from '../assets/image/pickoi1.png';
+import pickoi2 from '../assets/image/pickoi2.png';
+import pickoi3 from '../assets/image/pickoi3.png';
+import { useLocation } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
-const OrderInformation = ({ totalOrder, deliveryCost, totalAmount, deliveryTo }) => {
+const OrderInformation = () => {
+    const location = useLocation();
+    const { state } = location;
+    const orderData = state ? state.data : {};
+
+    const { totalPrice, startPlace, endPlace, deliveryType, boxes } = orderData;
+
+    // CHUYỂN SANG ĐƠN VỊ TIỀN TỂ LÀ VNĐ
+    const formatCurrency = (amount) => {
+        return amount.toLocaleString('vi-VN', { style: 'currency', currency: 'VND' });
+    };
+
+    const initialCount1 = boxes.find(box => box.boxid === 1)?.quantity || 0;
+    const initialCount2 = boxes.find(box => box.boxid === 2)?.quantity || 0;
+    const initialCount3 = boxes.find(box => box.boxid === 3)?.quantity || 0;
+
+    const [number1, setNumber1] = useState(initialCount1);
+    const [number2, setNumber2] = useState(initialCount2);
+    const [number3, setNumber3] = useState(initialCount3);
+
+    // Delivery cost calculation
+    const DeliveryType = () => {
+        return deliveryType === 1 ? 300000 : 850000;
+    };
 
     return (
         <>
             <div className="OrderInformation-paymentmethods">
-                <p>📦Your order is getting ready🚚 <br></br> 🌐🎏❤️</p>
+                <p>📦Your order is getting ready🚚 <br /> 🌐🎏❤️</p>
             </div>
             <div className="OrderInformation-main-order">
                 <div className="OrderInformation-main-pic">
                     <div className="OrderInformation-pic-koi1">
                         <img src={pickoi1} alt="PIC KOI 1" />
                         <div className="OrderInformation-pic1">
-                            <p>Large Box (S03)</p>
+                            <p>Large Box (S01)</p>
                             <div className="OrderInformation-info-box">
-                                <p>contains <br /> <span>10-20</span> Koi Fish totality</p>
-
+                                <p>Contains <br /> <span>10-20</span> Koi Fish totality</p>
                             </div>
                             <button className='OrderInformation-button'>Delete</button>
                         </div>
                         <div className="OrderInformation-amount1">
-                            <button className='decrement-button1'
-                            >
+                            <button className='decrement-button1' >
                                 -
                             </button>
-                            <button className='OrderInformation-count1'>
-                                
-                            </button>
-                            <button className='increment-button1'>
+                            <span className='OrderInformation-count1'>{number1}</span>
+                            <button className='increment-button1' >
                                 +
                             </button>
-
-
                         </div>
                     </div>
-
 
                     <div className="OrderInformation-pic-koi2">
                         <img src={pickoi2} alt="PIC KOI 2" />
                         <div className="OrderInformation-pic2">
                             <p>Medium Box (S02)</p>
                             <div className="OrderInformation-info-box">
-                                <p>contains at least <span>5-10</span> Koi Fish</p>
-
+                                <p>Contains at least <span>5-10</span> Koi Fish</p>
                             </div>
                             <button className='OrderInformation-button'>Delete</button>
                         </div>
                         <div className="OrderInformation-amount2">
-                            <button className='decrement-button2'
-                            >
+                            <button className='decrement-button2'>
                                 -
                             </button>
-                            <button className='OrderInformation-count2'>
-                                
-                            </button>
-                            <button className='increment-button2'>
+                            <span className='OrderInformation-count2'>{number2}</span>
+                            <button className='increment-button2' >
                                 +
                             </button>
                         </div>
                     </div>
+
                     <div className="OrderInformation-pic-koi3">
                         <img src={pickoi3} alt="PIC KOI 3" />
                         <div className="OrderInformation-pic3">
-                            <p>Small Box (S01)</p>
+                            <p>Small Box (S03)</p>
                             <div className="OrderInformation-info-box">
-                                <p>contains at least <span>3-5</span> Koi Fish maximum</p>
-
+                                <p>Contains at least <span>3-5</span> Koi Fish maximum</p>
                             </div>
                             <button className='OrderInformation-button'>Delete</button>
                         </div>
                         <div className="OrderInformation-amount3">
-                            <button className='decrement-button3'
-                            >
+                            <button className='decrement-button3'>
                                 -
                             </button>
-                            <button className='OrderInformation-count3'>
-                              
-                            </button>
+                            <span className='OrderInformation-count3'>{number3}</span>
                             <button className='increment-button3'>
                                 +
                             </button>
@@ -91,35 +99,34 @@ const OrderInformation = ({ totalOrder, deliveryCost, totalAmount, deliveryTo })
                     </div>
                 </div>
 
-
                 <div className="OrderInformation-order-information">
                     <div className='OrderInformation-order-details'>
                         <h2>Order Information</h2>
                         <div className='OrderInformation-line'></div>
-                        <p>Total order: {totalOrder} { parseFloat(1000000)} vnđ</p>
-                        <p>Delivery Cost: {deliveryCost} vnđ</p>
-                        <span>Total Amount: {totalAmount} { parseFloat(1000000)} vnđ</span>
-                        <p>Delivery to: {deliveryTo}</p>
+                        <p>Total order: {formatCurrency(totalPrice - DeliveryType())}</p>
+                        <p>shipping not included</p>
+                        <p>Delivery Cost: {formatCurrency(DeliveryType())} </p>
+                        <span>Total Amount: {formatCurrency(totalPrice)}</span>
+                        <p>Sent from: {startPlace}</p>
+                        <p>Delivery to: {endPlace}</p>
                         <p>By pressing the button, I agree to the <a href="#">Terms and Conditions.</a></p>
 
                         <Link to='/Payment'>
-                            <button className="OrderInformation-payment-button">{ parseFloat(100000000)} vnđ Payment
-                                <a href="#"><span>→</span></a></button>
+                            <button className="OrderInformation-payment-button">
+                                {formatCurrency(totalPrice)} Payment <span>→</span>
+                            </button>
                         </Link>
-                        <div className="OrderInformation-cancel">
-                            <a href='#'>Delete</a>
-                        </div>
+                        <Link to='/DeleteOrder'>
+                            <div className="OrderInformation-cancel">
+                                <a href='#'>Delete</a>
+                            </div>
+                        </Link>
+
                     </div>
-
                 </div>
-
-
             </div>
-
         </>
-
     );
-}
+};
 
-
-export default OrderInformation
+export default OrderInformation;
