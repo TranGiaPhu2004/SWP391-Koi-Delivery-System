cbd3e15ee5cc4c2309ef3caff5b7e607945d1edd
huyga154 <huyga154@gmail.com>
2024-10-23 11:28:32 +0700

    DB change fix api

--- BackEnd/src/main/java/com/example/demo/dto/response/ListOrderResponseDTO.java
+++ BackEnd/src/main/java/com/example/demo/dto/response/ListOrderResponseDTO.java
@@ -13,6 +13,8 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ListOrderResponseDTO implements Serializable {
+    
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private List<OrderDTO> orders;
 
     @JsonIgnore
